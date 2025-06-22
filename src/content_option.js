f179103af32be6681d434efa1ae7b1bf21ac6308
DukeDennis <P2666957@my365.dmu.ac.uk>
2025-06-21 17:41:15 -0700

    first commit

---
+++ src/content_option.js
@@ -0,0 +1,166 @@
+const logotext = "MAC";
+const meta = {
+    title: "Miguel Adrienne Corachea",
+    description: "I'm Miguel Adrienne Corachea ",
+};
+
+const introdata = {
+    title: "I’m Miguel Adrienne Corachea",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+        third: "I develop mobile apps",
+    },
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum",
+    your_img_url: "https://ik.imagekit.io/kobe2408/profile%20pic.png?updatedAt=1750540571192",
+};
+
+const dataabout = {
+    title: "about myself",
+    aboutme: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent quis dolor id ligula semper elementum feugiat pretium nulla. Nunc non commodo dolor. Nunc mollis dignissim facilisis. Morbi ut magna ultricies.",
+};
+const worktimeline = [{
+        jobtitle: "Designer of week",
+        where: "YAdfi",
+        date: "2020",
+    },
+    {
+        jobtitle: "Designer of week",
+        where: "Jamalya",
+        date: "2019",
+    },
+    {
+        jobtitle: "Designer of week",
+        where: "ALquds",
+        date: "2019",
+    },
+];
+
+const skills = [{
+        name: "Python",
+        value: 90,
+    },
+    {
+        name: "Djano",
+        value: 85,
+    },
+    {
+        name: "Javascript",
+        value: 90,
+    },
+    {
+        name: "React",
+        value: 75,
+    },
+    {
+        name: "Jquery",
+        value: 85,
+    },
+];
+
+const services = [{
+        title: "UI & UX Design",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+    {
+        title: "Mobile Apps",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+    {
+        title: "Wordpress Design",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://ik.imagekit.io/kobe2408/snkrview.png?updatedAt=1750466430609",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://ik.imagekit.io/kobe2408/nbahitormiss.png?updatedAt=1750466430559",
+        description: "A high-stakes game that puts players head-to-head to see who dominates and who fades. Only one can leave no doubt.",
+        link: "#",
+    },
+    {
+        img: "https://ik.imagekit.io/kobe2408/herculex.png?updatedAt=1750466430465",
+        description: "Where myth meets grit. A brand powered by strength, sculpted ambition, and the drive to lift beyond limits.",
+        link: "#",
+    },
+    {
+        img: "https://ik.imagekit.io/kobe2408/fctracker.png?updatedAt=1750466430454",
+        description: "Your market pulse, in real time. Stay ahead in crypto and forex with smart tools that help you monitor trends, track volatility, and make sharper trading moves.",
+        link: "#",
+    },
+    {
+        img: "https://ik.imagekit.io/kobe2408/revolv.png?updatedAt=1750466430480",
+        description: "Keep your rhythm in check—this tracker spins your daily routines into momentum that lasts.",
+        link: "#",
+    },
+    {
+        img: "https://ik.imagekit.io/kobe2408/logit.png?updatedAt=1750466430543",
+        description: "Your personal space to track thoughts, goals, and growth—designed for mindful reflection, one page at a time.",
+        link: "#",
+    },
+
+    {
+        img: "https://ik.imagekit.io/kobe2408/netsync.png?updatedAt=1750467680472",
+        description: "A hub where people connect, collaborate, and grow together—streamlining relationships in a fast-paced digital world.",
+        link: "#",
+    },
+    {
+        img: "https://ik.imagekit.io/kobe2408/skypulse.png?updatedAt=1750466430585",
+        description: "Weather worldwide, at a glance. From Honolulu to Auckland, SkyPulse delivers real-time forecasts so you're always a step ahead—no matter where life takes you.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/550/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "name@domain.com",
+    YOUR_FONE: "(555)123-4567",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com",
+    facebook: "https://facebook.com",
+    linkedin: "https://linkedin.com",
+    twitter: "https://twitter.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
